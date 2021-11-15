695fb23a2ee44f2cd9525cb80f954e52360d952c
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2021-11-15 16:55:40 +0100

    Heeel veel comments geschreven

--- notebooks/utils/scopeutil.py
+++ notebooks/utils/scopeutil.py
@@ -3,8 +3,8 @@ import pandas as pd
 # Nu hoef je alleen ene constante aan te roepen om je tijd te krijgen
 # Dan ben je minder kwetsbaar voor fouten
 STOF_KOPERCHLORIDE = "koper(ii)chloride"
-STOF_ACETOON = ""
-STOF_KOPERCHLORIDE_ACETOON = ""
+STOF_ACETOON = "nog om in te vullen (STOF_ACETOON)"
+STOF_KOPERCHLORIDE_ACETOON = "nog om in te vullen (STOF_KOPERCHLORIDE_ACETOON)"
 
 SI_PULSE = 1
 SI_SIGNAL = 2
@@ -39,6 +39,12 @@ METADATA = {
 
 
 def df_for_csv(path):
+    """
+    Leest een csv bestand op en zet het om in een dataframe.
+
+    :param path: het pad van de csv bestand om te lezen
+    :return: een dataframe
+    """
     df = pd.read_csv(path, skiprows=20)
     df = df.iloc[:, [3, 4]]
     df.columns = ["x", "y"]
@@ -46,14 +52,44 @@ def df_for_csv(path):
 
 
 def get_verhoudingen(stof):
+    """
+    Haalt de mogelijke verhoudingen op voor de specifieke stof.
+
+    :param stof: een van STOF_KOPERCHLORIDE, STOF_ACETOON of STOF_KOPERCHLORIDE_ACETOON
+    :return: een lijst van verhoudingen
+    """
     return METADATA[stof][K_VERHOUDINGEN]
 
 
 def get_delays(stof, verhouding, T):
+    """
+    Haalt de mogelijke delays op voor de specifieke stof, verhouding, T meting.
+
+    :param stof: een van STOF_KOPERCHLORIDE, STOF_ACETOON of STOF_KOPERCHLORIDE_ACETOON
+    :param verhouding: bijvoorbeeld "1_4", zie functie get_delays()
+    :param T: een van T1 of T2
+    :return: een lijst van delays
+    """
     return METADATA[stof][K_DELAY_INTERVALS][T][verhouding]
 
 
 def get_df(stof, verhouding, T, delay, signal_index, v=False):
+    """
+    Haalt de dataframe op volgens de parameters die jij meegeeft.
+
+    Bijvoorbeeld:
+
+    df_CuCl_1_16_T2_300us_signal = get_df(STOF_KOPERCHLORIDE, "1_16", T2, 300, SI_SIGNAL)
+    df_CuCl_1_16_T2_300us_signal.plot()
+
+    :param stof: een van STOF_KOPERCHLORIDE, STOF_ACETOON of STOF_KOPERCHLORIDE_ACETOON
+    :param verhouding: bijvoorbeeld "1_4", zie functie get_delays()
+    :param T: een van T1 of T2
+    :param delay: een delay waarde, haal ze op met de functie get_delays()
+    :param signal_index: een van SI_SIGNAL of SI_PULSE
+    :param v: verbose. Stel v=True als je extra prints() wilt krijgen om makkelijk fouten te kunnen opsporen.
+    :return: een dataframe
+    """
     filepath = filepath_for_measurement_params(stof, verhouding, T, delay, signal_index)
     if v:
         print(filepath)
@@ -61,21 +97,39 @@ def get_df(stof, verhouding, T, delay, signal_index, v=False):
 
 
 def get_df_of_all_delays(stof, verhouding, T, signal_index, v=False):
+    """
+    Haalt de dataframes op van alle delays.
+
+    Kun je gebruiken om bijvoorbeeld de hele reeks metingen in een klap op te halen:
+
+    meetreeks_CuCl_1_4_T1_signal = get_df_of_all_delays(STOF_KOPERCHLORIDE, "1_4", T1, SI_SIGNAL)
+    for (delay, df) in meetreeks_CuCl_1_4_T1_signal:
+        df.plot(title=f"Plot van meting op t={delay}us")
+
+    :param stof: een van STOF_KOPERCHLORIDE, STOF_ACETOON of STOF_KOPERCHLORIDE_ACETOON
+    :param verhouding: bijvoorbeeld "1_4", zie functie get_delays()
+    :param T: een van T1 of T2
+    :param signal_index: een van SI_SIGNAL of SI_PULSE
+    :param v: verbose. Stel v=True als je extra prints() wilt krijgen om makkelijk fouten te kunnen opsporen.
+    :return: een lijst van (delay, dataframe) tuples
+    """
     delays = get_delays(stof, verhouding, T)
     if v:
         print(delays)
-    return [get_df(stof, verhouding, T, delay, signal_index, v) for delay in delays]
+    return [(delay, get_df(stof, verhouding, T, delay, signal_index, v)) for delay in delays]
 
 
 def filepath_for_measurement_params(stof, verhouding, T, delay, signal_index):
     """
-    remember dat koper(ii)chloride veelal wordt aangegeven als cucl
+    Geeft je filepath (op je hardeschijf) waar de csv hoort te staan.
+    Remember dat koper(ii)chloride veelal wordt aangegeven als cucl
+
     :param stof: stof is een van de STOF_ constantes. Bijvoorbeeld STOF_KOPERCHLORIDE
     :param verhouding: verhouding is 1 of 'parameter'
     :param T: T is T1 of T2
     :param delay: delay is waar je begon - meestal 400
     :param signal_index: signal index is welke channel je hebt
-    :return:
+    :return: pad waar csv hoort te staan
     """
     return f"../data/cucl/verhouding {verhouding} {stof}/{T}/delay{delay}u00{signal_index}.csv"
 
