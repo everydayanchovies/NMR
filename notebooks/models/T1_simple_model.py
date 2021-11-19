5d36ccd151ef41b14252dd7d7c7796ba82b7dca1
floorvanemmerik <floor.emmerik@gmail.com>
2021-11-19 11:48:55 +0100

    EEN FITJEEE

--- notebooks/models/T1_simple_model.py
+++ notebooks/models/T1_simple_model.py
@@ -2,14 +2,19 @@
 import numpy as np
 import math
 from lmfit import models
+from numpy import log as ln
 
 # lambda functie waaraan je kan fitten
 # Dit is de gelineariseerde versie van de gegeven formule in de handleiding
-f = lambda evenwichtsmagnetisatie, magnetisatie_z, t: t / (
-    -1 * np.ln(1 - ((magnetisatie_z / evenwichtsmagnetisatie)))
+f = lambda t, evenwichtsmagnetisatie, magnetisatie_z: t / (
+    -1 * ln(1 - ((magnetisatie_z / evenwichtsmagnetisatie)))
 )
 
 model_eerstetijd = models.Model(f)
+
+test = lambda t, M0, T1: M0 * (1 - 2 * np.exp(-1 * (t / T1)))
+
+model_test = models.Model(test)
 
 # t = delays
 # magnetisatie_z = gemiddelde piek
