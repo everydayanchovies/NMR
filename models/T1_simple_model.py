0d0da513fea942f79859f6fe2fa68e5385ccfa21
floorvanemmerik <floor.emmerik@gmail.com>
2021-11-19 09:45:08 +0100

    lambda functie voor T1

---
+++ models/T1_simple_model.py
@@ -0,0 +1,12 @@
+# Hierbij enkele functies waarmee het mogelijk is om de data aan te fitten
+import numpy as np
+import math
+from lmfit import models
+
+# lambda functie waaraan je kan fitten
+# Dit is de gelineariseerde versie van de gegeven formule in de handleiding
+f = lambda evenwichtsmagnetisatie, magnetisatie_z, t: t / (
+    -1 * np.ln(1 - ((magnetisatie_z / evenwichtsmagnetisatie)))
+)
+
+model_T1 = models.model(f)
